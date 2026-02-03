abc60eef57db4b3358408dc30b9da9490550cffe
james841 <afolayanshem@gmai.com>
2026-02-03 14:30:01 +0100

    changes made

--- app/components/RichTextEditor.tsx
+++ app/components/RichTextEditor.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';  // ← Correct path for react-quill-new
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
